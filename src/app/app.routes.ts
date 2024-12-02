bf65d5be00cc388d786535aab9cea333388b0e31
CovIsaac <a278949@alumnos.uaslp.mx>
2024-12-01 23:29:46 -0600

    Añadido Sobre-nosotros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { MezcalListComponent } from './mezcal-list/mezcal-list.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
 
 export const routes: Routes = [
     {path: '', component: InicioComponent},
-    {path: 'sabores', component: MezcalListComponent}
+    {path: 'sabores', component: MezcalListComponent},
+    {path: 'sobre-nosotros', component: SobreNosotrosComponent}
 ];
